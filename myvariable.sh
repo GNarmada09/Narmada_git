ef3b9a2f3d39cb5f2f4e2d490fdd86ab9dc6372a
Narmada <nammu9@gmail.com>
2019-08-26 12:20:45 +0530

    Created My variable file

---
+++ myvariable.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+###############################################
+#Purpose  :  How to create variable and its values
+#Owner    :  Narmada
+#Input    :  None
+#Output   :  Print some message on script
+#version  :  1.0
+###############################################
+
+#clear the screan
+clear
+
+
+#print welcome Devops (Scripting and variables)
+echo " welcome to Bash scripting "
+
+
+sleep 3 # wait for 3 sec
+
+# define the variable 
+
+YEAR=1990
+COMPNAME=Narmada_Devops
+
+# print variable 
+
+echo "$YEAR"
+echo '$COMPNAME'
+
+
+# print end message
+echo ' End by Narmada '
+
+
+
+
+ 
