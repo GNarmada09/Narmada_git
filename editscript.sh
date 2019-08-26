3df1e525d8b1a5249d702baf1f9c017977e055d7
Narmada <nammu9@gmail.com>
2019-08-26 16:08:14 +0530

    text for use all files for edit

---
+++ editscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+###############################################
+#purpose  :  To learn Shell Scripts
+#Owner    :  Narmada
+#Input    :  None
+#Output   :  Print some message on script
+#version  :  1.0
+###############################################
+
+
+
+
+
+
+
+
+
+
+# print end message
+
+echo 'End by narmada'
+
+ 
