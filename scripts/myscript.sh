fa593f26ff1459c1d14d4c6123461c5c2b98b66e
Narmada Gutta <narmadagutta@git.com>
2019-08-26 09:50:27 +0530

    added New Script file

---
+++ scripts/myscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+###############################################
+#purpose  :  To learn Shell Scripts
+#Owner    :  Narmada
+#Input    :  None
+#Output   :  Print some message on script
+#version  :  1.0
+###############################################
+
+#clear the screan
+clear
+
+
+#print welcome Devops (Scripting)
+echo ' welcome to Bash scripting '
+
+
+sleep 5 # wait for 3 sec
+
+# print end message
+
+echo 'End by narmada'
+
+ 
