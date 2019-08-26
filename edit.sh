1f5b31e3d71e3e64f1a99480c979a3eea7c80e6e
Narmada <nammu9@gmail.com>
2019-08-26 19:37:46 +0530

    to create new script

---
+++ edit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+###############################################
+#Purpose  :  To create 
+#Owner    :  Narmada
+#Input    :  None
+#Output   :  Print some message on script
+#version  :  1.0
+###############################################
+
+
+
+
+
+
+
+
+# print end message
+echo ' End by Narmada '
+
+
+
+
+ 
